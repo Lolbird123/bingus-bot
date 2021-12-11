90d1cfc78e6b08d72b4fdd34d8e84f7959513c5e
Lolbird123 <65594081+Lolbird123@users.noreply.github.com>
2021-12-11 21:55:47 +0000

    green names :)
    uwu

--- filter.js
+++ filter.js
@@ -1,9 +1,15 @@
 const discord = require('discord.js');
 
 function exec(vars) {
+	var comps = vars.msg.content.toLowerCase();
+	
 	if(vars.msg.content.toLowerCase() === 'ñ' && vars.msg.guild.id === vars.config.guild && !vars.msg.member.roles.cache.has(vars.config.roles.lvl5)) {
 		vars.msg.channel.send(`<@!${vars.msg.author.id}> dumbass`);
 		vars.msg.delete();
+	}
+	if((comps.has('nitro') || comps.has('steam')) && vars.msg.guild.id === vars.config.guild && !vars.msg.member.roles.cache.has(vars.config.roles.lvl5)) {
+		vars.msg.channel.send(`<@!${vars.msg.author.id}> dumbass`);
+                vars.msg.delete();
 	}
 	if(vars.msg.content.toLowerCase() === 'bingus') vars.msg.react('<:bingus:764103384046239754>');
 }
