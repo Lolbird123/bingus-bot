ab75dc0ccb3934565aac70c3514e472bf331c29d
Lolbird123 <65594081+Lolbird123@users.noreply.github.com>
2021-12-31 00:20:39 +0000

    make output parse objects properly
    JSON.parse(); my beloved

--- cmds/eval.js
+++ cmds/eval.js
@@ -13,22 +13,30 @@ function exec(args, vars) {
 		try {
 			var code = args.join(' ');
 			var evaled = eval(code);
-			var embed = new discord.MessageEmbed()
-			.setTitle('Success')
-			.setColor('#00ff00')
-			.setDescription(`\`\`\`\n${evaled}\n\`\`\``);
-			msg.reply({embeds:[embed]});
+			var embed = new discord.MessageEmbed();
+			embed.setTitle('Success');
+			embed.setColor('#00ff00');
+			if(typeof evaled === 'object') {
+        embed.setDescription(`\`\`\`\n${JSON.stringify(evaled)}\n\`\`\``);
+      } else {
+        embed.setDescription(`\`\`\`\n${evaled}\n\`\`\``);
+      }
+      msg.reply({embeds:[embed]});
 		} catch(err) {
-			var embed = new discord.MessageEmbed()
-			.setTitle('Error')
-			.setColor('#ff0000')
-			.setDescription(`\`\`\`\n${err}\n\`\`\``);
+			var embed = new discord.MessageEmbed();
+			embed.setTitle('Error');
+			embed.setColor('#ff0000');
+			if(typeof err === 'object') {
+        embed.setDescription(`\`\`\`\n${JSON.stringify(err)}\n\`\`\``);
+      } else {
+        embed.setDescription(`\`\`\`\n${err}\n\`\`\``);
+      }
 			msg.reply({embeds:[embed]});
 		}
 	} else {
-		var embed = new discord.MessageEmbed()
-		.setTitle('Access Denied')
-		.setColor('#ff0000');
+		var embed = new discord.MessageEmbed();
+		embed.setTitle('Access Denied');
+		embed.setColor('#ff0000');
 		msg.reply({embeds:[embed]});
 	}
 }
