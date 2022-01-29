ee9c43e300a073b647948ae3330f8b37e590a6e1
Lolbird123 <65594081+Lolbird123@users.noreply.github.com>
2022-01-29 05:27:31 +0000

    uwu

--- filter.js
+++ filter.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 
 function exec(vars) {
 	var comps = vars.msg.content.toLowerCase();
-	
+
 	if(vars.msg.content.toLowerCase() === 'ñ' && vars.msg.guild.id === vars.config.guild && !vars.msg.member.roles.cache.has(vars.config.roles.lvl5)) {
 		vars.msg.channel.send(`<@!${vars.msg.author.id}> dumbass`);
 		vars.msg.delete();
@@ -12,6 +12,15 @@ function exec(vars) {
                 vars.msg.delete();
 	}
 	if(vars.msg.content.toLowerCase() === 'bingus') vars.msg.react('<:bingus:764103384046239754>');
+  //the funny words are stored in config
+  if(vars.msg.guild.id === vars.config.guild) {
+    for(var i=0; i<vars.config.words.length; i++) {
+      if(comps.includes(vars.config.words[i]) {
+        vars.msg.channel.send(`<@!${vars.msg.author.id}> check your roles uwu`);
+        vars.msg.member.roles.add(vars.config.roles.uwu);
+      }
+    }
+  }
 }
 
 module.exports = exec;
