a0f375f37141e3d83fb7abbcadaafdfdc7bdb957
Lolbird123 <65594081+Lolbird123@users.noreply.github.com>
2022-03-11 08:38:34 +0000

    how the fuck did the last version work

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,4 +1,4 @@
-const discord = require('discord.js');
+const Discord = require('discord.js');
 
 const info = {
 	name: 'eval',
@@ -13,28 +13,44 @@ function exec(args, vars) {
 		try {
 			var code = args.join(' ');
 			var evaled = eval(code);
-			var embed = new discord.MessageEmbed();
-			embed.setTitle('Success');
-			embed.setColor('#00ff00');
-			if(typeof evaled === 'object') {
-        embed.setDescription(`\`\`\`\n${JSON.stringify(evaled)}\n\`\`\``);
+
+      if(typeof evaled === 'object' && evaled instanceof Promise) {
+        msg.react('🕔');
+        evaled.then((data) => {
+          var embed = new Discord.MessageEmbed();
+          embed.setTitle('Success (Promise Result)');
+          embed.setColor('#00ff00');
+          if(typeof data === 'object') {
+            embed.setDescription(`\`\`\`\n${JSON.stringify(data)}\n\`\`\``);
+          } else {
+            embed.setDescription(`\`\`\`\n${data}\n\`\`\``);
+          }
+          msg.reply({embeds:[embed]});
+        });
       } else {
-        embed.setDescription(`\`\`\`\n${evaled}\n\`\`\``);
+        var embed = new Discord.MessageEmbed();
+        embed.setTitle('Success');
+        embed.setColor('#00ff00');
+        if(typeof evaled === 'object') {
+          embed.setDescription(`\`\`\`\n${JSON.stringify(evaled)}\n\`\`\``);
+        } else {
+          embed.setDescription(`\`\`\`\n${evaled}\n\`\`\``);
+        }
+        msg.reply({embeds:[embed]});
       }
-      msg.reply({embeds:[embed]});
-		} catch(err) {
-			var embed = new discord.MessageEmbed();
-			embed.setTitle('Error');
-			embed.setColor('#ff0000');
-			if(typeof err === 'object') {
-        embed.setDescription(`\`\`\`\n${JSON.stringify(err)}\n\`\`\``);
+    } catch(err) {
+      var embed = new Discord.MessageEmbed();
+      embed.setTitle('Error');
+      embed.setColor('#ff0000');
+      if(typeof evaled === 'object') {
+        embed.setDescription(`\`\`\`\n${err.toString()}\n\`\`\``);
       } else {
         embed.setDescription(`\`\`\`\n${err}\n\`\`\``);
       }
-			msg.reply({embeds:[embed]});
-		}
+      msg.reply({embeds:[embed]});
+    }
 	} else {
-		var embed = new discord.MessageEmbed();
+		var embed = new Discord.MessageEmbed();
 		embed.setTitle('Access Denied');
 		embed.setColor('#ff0000');
 		msg.reply({embeds:[embed]});
